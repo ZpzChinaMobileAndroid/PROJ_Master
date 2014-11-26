98e510a091d8961549731716eab708423e752df8
d76ty76rtf <admin@AdmindeMacBook-Pro.local>
2014-11-26 09:53:37 +0800

    dds

--- gen/com/zhongji/master/android/phone/R.java
+++ gen/com/zhongji/master/android/phone/R.java
@@ -67,61 +67,92 @@ public final class R {
         public static final int slidingmenu_offset=0x7f050009;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int login_anonymity=0x7f020001;
-        public static final int login_background=0x7f020002;
-        public static final int login_headline=0x7f020003;
-        public static final int login_landing=0x7f020004;
-        public static final int login_password=0x7f020005;
-        public static final int login_register=0x7f020006;
-        public static final int login_user=0x7f020007;
-        public static final int register_agree=0x7f020008;
-        public static final int register_back=0x7f020009;
-        public static final int register_fond=0x7f02000a;
-        public static final int register_register=0x7f02000b;
-        public static final int register_top=0x7f02000c;
+        public static final int base_layout_bottom_rabtn_center_selector=0x7f020000;
+        public static final int base_layout_bottom_rabtn_message_selector=0x7f020001;
+        public static final int base_layout_bottom_rabtn_qs_selector=0x7f020002;
+        public static final int base_layout_bottom_rabtn_tag_selector=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_qs_l=0x7f020005;
+        public static final int ic_qs_n=0x7f020006;
+        public static final int ic_tag_l=0x7f020007;
+        public static final int ic_tag_n=0x7f020008;
+        public static final int ic_user_l=0x7f020009;
+        public static final int ic_user_n=0x7f02000a;
+        public static final int ic_xinxi_l=0x7f02000b;
+        public static final int ic_xinxi_n=0x7f02000c;
+        public static final int login_anonymity=0x7f02000d;
+        public static final int login_background=0x7f02000e;
+        public static final int login_headline=0x7f02000f;
+        public static final int login_landing=0x7f020010;
+        public static final int login_password=0x7f020011;
+        public static final int login_register=0x7f020012;
+        public static final int login_user=0x7f020013;
+        public static final int register_agree=0x7f020014;
+        public static final int register_back=0x7f020015;
+        public static final int register_fond=0x7f020016;
+        public static final int register_register=0x7f020017;
+        public static final int register_top=0x7f020018;
     }
     public static final class id {
-        public static final int bt_landing=0x7f080004;
-        public static final int bt_register=0x7f080005;
-        public static final int et_cellphone=0x7f08000a;
-        public static final int et_username=0x7f080002;
-        public static final int et_userpassword=0x7f080003;
-        public static final int et_usersecendpassword=0x7f08000c;
-        public static final int et_verification=0x7f08000b;
-        public static final int im_back=0x7f080009;
-        public static final int im_clause=0x7f08000d;
-        public static final int im_countersign=0x7f08000e;
-        public static final int login_anonymity=0x7f080000;
-        public static final int login_headline=0x7f080001;
-        public static final int textView1=0x7f080008;
-        public static final int textview=0x7f080006;
-        public static final int tv_foundpassword=0x7f080007;
+        public static final int advertview=0x7f080001;
+        public static final int base_layout_view=0x7f080011;
+        public static final int base_ragroup_bottom=0x7f080012;
+        public static final int bt_landing=0x7f080006;
+        public static final int bt_register=0x7f080007;
+        public static final int et_cellphone=0x7f08000b;
+        public static final int et_username=0x7f080004;
+        public static final int et_userpassword=0x7f080005;
+        public static final int et_usersecendpassword=0x7f08000e;
+        public static final int et_verification=0x7f08000c;
+        public static final int im_back=0x7f08000a;
+        public static final int im_clause=0x7f08000f;
+        public static final int im_countersign=0x7f080010;
+        public static final int im_send=0x7f08000d;
+        public static final int login_anonymity=0x7f080002;
+        public static final int login_headline=0x7f080003;
+        public static final int radio0=0x7f080013;
+        public static final int radio1=0x7f080014;
+        public static final int radio2=0x7f080015;
+        public static final int radio3=0x7f080016;
+        public static final int textView1=0x7f080000;
+        public static final int textview=0x7f080008;
+        public static final int tv_foundpassword=0x7f080009;
+        public static final int tv_left=0x7f080018;
+        public static final int tv_right=0x7f080019;
+        public static final int tv_title=0x7f080017;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_register=0x7f030002;
+        public static final int activity_company=0x7f030000;
+        public static final int activity_contacts=0x7f030001;
+        public static final int activity_launch_guide=0x7f030002;
+        public static final int activity_launch_launch=0x7f030003;
+        public static final int activity_login=0x7f030004;
+        public static final int activity_main=0x7f030005;
+        public static final int activity_product=0x7f030006;
+        public static final int activity_project=0x7f030007;
+        public static final int activity_register=0x7f030008;
+        public static final int base_activity_index=0x7f030009;
+        public static final int base_layout_bottom=0x7f03000a;
+        public static final int base_layout_top=0x7f03000b;
     }
     public static final class string {
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
         /**  下拉刷新 
          */
-        public static final int get_more=0x7f06000a;
+        public static final int get_more=0x7f060009;
         public static final int hello_world=0x7f060002;
-        public static final int index_ask_str=0x7f060007;
-        public static final int index_center_str=0x7f060009;
-        public static final int index_message_str=0x7f060008;
+        public static final int index_center_str=0x7f060008;
+        public static final int index_message_str=0x7f060007;
         /**  模块首页文章 
          */
         public static final int index_qa_str=0x7f060005;
         public static final int index_tag_str=0x7f060006;
         public static final int please_wait_str=0x7f060003;
-        public static final int pull_to_refresh=0x7f06000c;
-        public static final int refreshing=0x7f06000d;
-        public static final int release_to_refresh=0x7f06000b;
-        public static final int updating=0x7f06000e;
+        public static final int pull_to_refresh=0x7f06000b;
+        public static final int refreshing=0x7f06000c;
+        public static final int release_to_refresh=0x7f06000a;
+        public static final int updating=0x7f06000d;
         public static final int xunfei_speech_app_id=0x7f060004;
     }
     public static final class style {
